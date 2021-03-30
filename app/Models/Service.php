2fff51375e407de10f01c5df022fb58954ad9f51
senbiki akifumi <akifumi.senbiki.1209@gmail.com>
2021-03-30 10:39:13 +0900

    サービスモデルを修正

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -15,12 +15,18 @@ class Service extends Model
         "capacity",
         "price",
         "price_per_hour",
+        "memo",
         "is_displayed",
         "is_deleted",
         // 作成者
         "created_by",
         // 更新者
         "updated_by",
+    ];
+
+    protected $appends = [
+        "created_at_ja",
+        "updated_at_ja",
     ];
 
     public function reserves()
@@ -31,5 +37,17 @@ class Service extends Model
     public function owner()
     {
         return $this->hasOne(Owner::class, "id", "owner_id");
+    }
+
+    // レコード作成日時
+    public function getCreatedAtJaAttribute()
+    {
+        return $this->created_at->format("Y年n月j日")."<br>".$this->created_at->format("H時i分:s秒");
+    }
+
+    // レコード更新日時
+    public function getUpdatedAtJaAttribute()
+    {
+        return $this->updated_at->format("Y年n月j日")."<br>".$this->updated_at->format("H時i分:s秒");
     }
 }
